00ec08748641603a607aef162038ab3fdef4d029
enzcarix7 <enzcarix7@icloud.com>
2025-04-30 13:27:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ DB_PATH = os.getenv('DB_PATH', 'default_db_path')  # default if variable not exi
 
 # Email configuration
 EMAIL_SERVER = os.getenv('EMAIL_SERVER', 'smtp.example.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))  # Converti in intero
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))  # Convert int val
 SENDER_EMAIL = os.getenv('SENDER_EMAIL', 'your_email@example.com')
 RECEIVER_EMAIL = os.getenv('RECEIVER_EMAIL', 'receiver_email@example.com')
