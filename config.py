58ff87b3a10d0c00d0b0f6c82955ad994239b803
enzcarix7 <enzcarix7@icloud.com>
2025-04-30 13:27:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-# Configuration file for SIEM system
+import os
 
 # Database path
-DB_PATH = 'database.db'
+DB_PATH = os.getenv('DB_PATH', 'default_db_path')  # default if variable not exist
 
 # Email configuration
-EMAIL_SERVER = 'smtp.example.com'  # replace with your SMTP server
-EMAIL_PORT = 587
-SENDER_EMAIL = 'your_email@example.com'
-RECEIVER_EMAIL = 'receiver_email@example.com'
+EMAIL_SERVER = os.getenv('EMAIL_SERVER', 'smtp.example.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))  # Converti in intero
+SENDER_EMAIL = os.getenv('SENDER_EMAIL', 'your_email@example.com')
+RECEIVER_EMAIL = os.getenv('RECEIVER_EMAIL', 'receiver_email@example.com')
